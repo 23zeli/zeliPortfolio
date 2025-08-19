188799cdde1549acf198d20cd17c336cc24b1dc8
Mtutuzeli Ngobo <mtutuzeli.ngobo@gmail.com>
2025-08-19 11:55:30 +0200

    changed h3 to p

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -2,10 +2,31 @@ import React from 'react'
 
 function Cards(props) {
   return (
-    <div className='Cards' style={{color: "black"}}> 
-      <img className='card-image' src={props.getImageSrc} height={200}/>
-      <h2 className="card-header" >{props.title}</h2>
-      <h3 className="card-text" >{props.description}</h3>
+    <div className='Cards' style={{color: "black"}}>
+      <div className="card-image">
+        <img
+          src={props.getImageSrc}
+          alt={props.title}
+          // height={200}
+        />
+      </div>
+      <div className="card-text-container">
+        <h2 className="card-header" >{props.title}</h2>
+        <p className="card-text" >{props.description}</p>
+        <div className="skills-icons"></div>
+        <div className="links-btns" style={{display: 'flex'}}>
+          <a className="live-icon" type='button'href='#' target='blank'>
+            <img
+              src=''
+              alt='Live'
+              title='Live'
+              // style={{width: '25px', height: '25px',}}
+            />
+            <span style={{ color: 'black' }}>Live</span>
+          </a>
+          <a className="source-icon">Source</a>
+        </div>
+      </div>
     </div>
   )
 }
