c2878f8838cc7df0deecc7d9c602a0451e3bf1de
Mtutuzeli Ngobo <mtutuzeli.ngobo@gmail.com>
2025-08-20 14:24:57 +0200

    added skills icon

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -17,8 +17,13 @@ function Cards(props) {
       <div className="card-text-container">
         <h2 className="card-header" >{props.title}</h2>
         <p className="card-text" >{props.description}</p>
-        <div className="skills-icons"></div>
-         <div className="links-btns" style={{display: 'flex'}}>
+        <div className="skills-icons">
+          <span>React.js</span>
+          <span>JavaScript</span>
+          <span>HTML5</span>
+          <span>CSS3</span>
+        </div>
+        <div className="links-btns" style={{display: 'flex'}}>
           <a className="live-icon" type='button' href='#' target='blank' style={{marginRight: '10px'}}>
             <FontAwesomeIcon icon={faExternalLinkAlt} size='xs' className='external-link'/>
             <span>Live</span>
