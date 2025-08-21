75fa67b70dc01eda6e73a48ef7f667adac300d24
Mtutuzeli Ngobo <mtutuzeli.ngobo@gmail.com>
2025-08-21 09:38:44 +0200

    imported avatar

--- src/Components/LandingSection.jsx
+++ src/Components/LandingSection.jsx
@@ -1,63 +1,5 @@
 import React from "react";
-
-
-
-// const LandingSection = () => {
-//     <div className="FullScreenSection"
-//         // justifyContent="centre" alignItems="centre" isDarkBackGround backgroundColor="#18181b"
-//     >
-//         <div className="LandingSection"
-//             // spacing={16}
-//         >
-//             <div
-//                 className="Avatar"
-//                 // spacing={4}
-//                 // alignItems="centre"
-//             >
-//                 <img
-//                     src=""
-//                     // size="2xl"
-//                     name=""
-//                 />
-//                 <h4
-//                     // size="md"
-//                     // noOfLine={1}
-//                 >
-//                     Hello, I am Mtutuzeli!
-//                 </h4>
-//             </div>
-//             <div className="Text">
-//                 <h1
-//                     // size="3xl" noOfLine={1}
-//                 >
-//                     A frontend Developer
-//                 </h1>
-//                 <h1
-//                     // size="3xl" noOfLine={1}
-//                 >
-//                     specialised in React
-//                 </h1>
-//             </div>
-//         </div>
-//     </div>
-
-//     return(
-//         <div
-//             // backgroundColor=""
-//             // color={isDarkBackGround ? "white" : "black"}
-//         >
-//             <div
-//                 // maxWidth="1280px" minHeight="100vh" {...boxProps}
-//             >
-//                 {/* {children} */}
-//             </div>
-//         </div>
-//     )
-// }
-// function Logo(props) {
-//     const profilePic = <img src={moon} />;
-//     return profilePic;
-// }
+import avatarImage from '../assets/images/Avatar.jpg';
 
 function LandingSection() {
     const landingStyle = {
@@ -72,7 +14,7 @@ function LandingSection() {
 
     return(
         <div className="LandingSection" style ={landingStyle}>
-            <img className="avatar" src="./images/Avatar.jpg" height={200} />
+            <img className="avatar" id='avatar' src={avatarImage} alt="Avatar" height={200}/>
             <h4 style={{paddingBottom: "50px"}}>
                 Hello, I am Mtutuzeli!
             </h4>
